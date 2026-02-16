d2b01388a95248be72e9ec6f75c253b607e93528
FarisF2 <24_faris@student.smkti.net>
2026-02-17 00:51:29 +0800

    2/17/26

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,31 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <a href="{{ url('/portofolio') }}"
+                    class="inline-block px-4    lpy-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
+                        Buka Halaman Portofolio
+                        </a> 
+                </div>
+            </div>
+        </div>
+    </div>
+
+   <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\PortfolioController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\ComponentController;
 use App\Http\Controllers\FormLogin;
+use App\Http\Controllers\ProfileController;
+
 
 Route::get('/', [PortfolioController::class, 'index']);
 
@@ -24,9 +26,36 @@ Route::post('/login', [FormLogin::class, 'prosesLogin'])->name('login.process');
 
 Route::get('/logout', [FormLogin::class, 'logout'])->name('logout');
 
-Route::get('/portfolio', [PortfolioController::class, 'index'])->name('portfolio');
+Route::get('/portofolio', [PortfolioController::class, 'index'])->name('portfolio');
 
 Route::get('/projects', [App\Http\Controllers\ProjectController::class, 'index']);
 
 Route::get('/projects/debug', [App\Http\Controllers\ProjectController::class, 'debug'])->name('projects.debug');
 Route::get('/projects/json', [App\Http\Controllers\ProjectController::class, 'json'])->name('projects.json');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
+});
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
